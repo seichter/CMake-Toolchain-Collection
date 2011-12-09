28d54ba098debeff147674e37d8047124e87ccc5
Hartmut Seichter <hartmut@gmail.com>
2011-12-09 16:40:49 +0100

    Added a new toolchain that targets a Android generated standalone toolchain. Please note that this is different from using the NDK itself as a standalone toolchain. No STL and STL runtime chosing is supported

--- toolchain-android-ndk-r7.cmake
+++ toolchain-android-ndk-r7.cmake
@@ -7,7 +7,7 @@
 #
 
 # need to know where the NDK resides
-set(ANDROID_NDK_ROOT "$ENV{NDK_ROOT}" CACHE PATH "Android NDK location")
+set(ANDROID_NDK_ROOT "$ENV{NDK_TOOLCHAIN_ROOT}" CACHE PATH "Android Standalone Toolchain location")
 
 # set(ANDROID_NDK_TOOLCHAIN_DEBUG ON)
 
@@ -40,7 +40,6 @@ set(ANDROID_NDK_ABI)
 set(ANDROID_NDK_ABI_EXT)
 set(ANDROID_NDK_GCC_PREFIX)
 
-
 set(ANDROID_NDK_ARCH_CFLAGS)
 set(ANDROID_NDK_ARCH_LDFLAGS)
 
@@ -48,7 +47,7 @@ if("${ANDROID_NDK_ARCH}" STREQUAL "arm" )
 	set(ANDROID_NDK_ABI "armeabi")
 	set(ANDROID_NDK_ABI_EXT "arm-linux-androideabi")
 	set(ANDROID_NDK_GCC_PREFIX "arm-linux-androideabi")
-	set(ANDROID_NDK_ARCH_CFLAGS "-D__ARM_ARCH_5__ -D__ARM_ARCH_5T__ -D__ARM_ARCH_5E__ -D__ARM_ARCH_5TE__  -Wno-psabi -march=armv5te -mtune=xscale -msoft-float -mthumb")
+	set(ANDROID_NDK_ARCH_CFLAGS "-D__ARM_ARCH_5__ -D__ARM_ARCH_5T__ -D__ARM_ARCH_5E__ -D__ARM_ARCH_5TE__ -Wno-psabi -march=armv5te -mtune=xscale -msoft-float -mthumb")
 endif()	
 if("${ANDROID_NDK_ARCH}" STREQUAL "armv7" )
 	set(ANDROID_NDK_ABI "armeabi-v7a")
@@ -70,7 +69,7 @@ if(ANDROID_NDK_TOOLCHAIN_DEBUG)
 endif()
 
 # global C flags
-set(ANDROID_NDK_GLOBAL_CFLAGS "-fomit-frame-pointer -fno-strict-aliasing -finline-limit=64")
+set(ANDROID_NDK_GLOBAL_CFLAGS "-fpic -fomit-frame-pointer -fno-strict-aliasing -finline-limit=64 -ffunction-sections -funwind-tables -fstack-protector")
 
 
 # choose NDK STL implementation
@@ -96,30 +95,45 @@ endif()
 # set version
 set(ANDROID_NDK_GCC_VERSION "4.4.3")
 
-
 # STL
 set(ANDROID_NDK_STL_CXXFLAGS)
 set(ANDROID_NDK_STL_LIBRARYPATH)
 set(ANDROID_NDK_STL_LDFLAGS)
 if ("${ANDROID_NDK_STL}" STREQUAL "stlport") 
-	set(ANDROID_NDK_STL_CXXFLAGS "-D_STLP_USE_NEWALLOC -I${ANDROID_NDK_ROOT}/sources/cxx-stl/${ANDROID_NDK_STL}/stlport -fno-exceptions -fno-rtti")
+	set(ANDROID_NDK_STL_CXXFLAGS "-D_STLP_USE_SIMPLE_NODE_ALLOC -D_STLP_NO_EXCEPTIONS  -fno-exceptions -fno-rtti -I${ANDROID_NDK_ROOT}/sources/cxx-stl/${ANDROID_NDK_STL}/stlport") #-D_STLP_USE_NEWALLOC
 	set(ANDROID_NDK_STL_LIBRARYPATH "${ANDROID_NDK_ROOT}/sources/cxx-stl/${ANDROID_NDK_STL}/libs/${ANDROID_NDK_ABI}")
-	set(ANDROID_NDK_STL_LDFLAGS "-lstlport_static")
+	set(ANDROID_NDK_STL_LDFLAGS "-lstdc++")
 else()
-	set(ANDROID_NDK_STL_CXXFLAGS "-I${ANDROID_NDK_ROOT}/sources/cxx-stl/${ANDROID_NDK_STL}/include -I${ANDROID_NDK_ROOT}/sources/cxx-stl/${ANDROID_NDK_STL}/libs/${ANDROID_NDK_ABI}/include" FORCE)
-	set(ANDROID_NDK_STL_LIBRARYPATH "${ANDROID_NDK_ROOT}/sources/cxx-stl/${ANDROID_NDK_STL}/libs/${ANDROID_NDK_ABI}" FORCE)
-	set(ANDROID_NDK_STL_LDFLAGS " -lsupc++ -lgnustl_static")
+	set(ANDROID_NDK_STL_CXXFLAGS "-I${ANDROID_NDK_ROOT}/sources/cxx-stl/${ANDROID_NDK_STL}/include -I${ANDROID_NDK_ROOT}/sources/cxx-stl/${ANDROID_NDK_STL}/libs/${ANDROID_NDK_ABI}/include  -fno-exceptions -fno-rtti ")
+	set(ANDROID_NDK_STL_LIBRARYPATH "${ANDROID_NDK_ROOT}/sources/cxx-stl/${ANDROID_NDK_STL}/libs/${ANDROID_NDK_ABI}")
+	set(ANDROID_NDK_STL_LDFLAGS "-lstdc++")
 endif()
 
 # global linker flags
+set(ANDROID_NDK_GLOBAL_LDFLAGS "-Wl,--no-undefined -Wl,-z,noexecstack -Wl,--gc-sections -Wl,-z,nocopyreloc")
 
-set(ANDROID_NDK_GLOBAL_LDFLAGS "-Wl,--no-undefined -Wl,-z,noexecstack")
+# get the gcc companion lib
+exec_program(
+	${ANDROID_NDK_ROOT}/toolchains/${ANDROID_NDK_ABI_EXT}-${ANDROID_NDK_GCC_VERSION}/prebuilt/${ANDROID_NDK_HOST}/bin/${ANDROID_NDK_GCC_PREFIX}-gcc 
+	ARGS "-mthumb-interwork -print-libgcc-file-name"
+	OUTPUT_VARIABLE ANDROID_NDK_GCC_COMPANIONLIBRARY
+	)
 
+get_filename_component(ANDROID_NDK_GCC_COMPANIONLIBRARY_PATH ${ANDROID_NDK_GCC_COMPANIONLIBRARY} PATH)
+#set(CMAKE_CXX_IMPLICIT_LINK_DIRECTORIES "${ANDROID_NDK_GCC_COMPANIONLIBRARY_PATH}")
+
+# hack
+#add_definitions(-static-libgcc)
+#link_libraries(${ANDROID_NDK_GCC_COMPANIONLIBRARY})
+
+#set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -lstdc++ -lstlport_static ${ANDROID_NDK_GCC_COMPANIONLIBRARY}" CACHE STRING "Linker flags" FORCE)
+#set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -lstdc++ -lstlport_static ${ANDROID_NDK_GCC_COMPANIONLIBRARY}" CACHE STRING "Linker flags" FORCE)
 
 # some overrides (see docs/STANDALONE-TOOLCHAIN.html) 
-set(CMAKE_C_FLAGS "${ANDROID_NDK_GLOBAL_CFLAGS} ${ANDROID_NDK_GLOBAL_LDFLAGS} --sysroot=${ANDROID_NDK_SYSROOT} -DANDROID ${ANDROID_NDK_ARCH_CFLAGS} ${ANDROID_NDK_ARCH_LDFLAGS} -nostdlib -llog -landroid -llog -lc -lm" CACHE STRING "C flags" FORCE)
-set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} ${ANDROID_NDK_STL_CXXFLAGS} -L${ANDROID_NDK_STL_LIBRARYPATH} -lstdc++ ${ANDROID_NDK_STL_LDFLAGS}" CACHE STRING "C++ flags" FORCE)
+set(CMAKE_C_FLAGS "-v -MMD -MP -MF ${ANDROID_NDK_GLOBAL_CFLAGS} ${ANDROID_NDK_GLOBAL_LDFLAGS} --sysroot=${ANDROID_NDK_SYSROOT} -DANDROID ${ANDROID_NDK_ARCH_CFLAGS} ${ANDROID_NDK_ARCH_LDFLAGS} -nostdlib -landroid -llog -lc -lm -lgcc" CACHE STRING "C flags" FORCE)
+set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} ${ANDROID_NDK_STL_CXXFLAGS} -L${ANDROID_NDK_STL_LIBRARYPATH} ${ANDROID_NDK_STL_LDFLAGS}" CACHE STRING "C++ flags" FORCE)
 
+#message(STATUS "${ANDROID_NDK_GCC_COMPANIONLIBRARY_PATH}")
 
 # specify compiler
 set(CMAKE_C_COMPILER   "${ANDROID_NDK_ROOT}/toolchains/${ANDROID_NDK_ABI_EXT}-${ANDROID_NDK_GCC_VERSION}/prebuilt/${ANDROID_NDK_HOST}/bin/${ANDROID_NDK_GCC_PREFIX}-gcc" CACHE PATH "C compiler" FORCE)
