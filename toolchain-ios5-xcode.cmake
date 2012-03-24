35fbae834d836342caa27408fe9f5852802afa59
Hartmut Seichter <hartmut@gmail.com>
2012-03-24 01:08:08 +0100

    fix for iOS5 and Xcode 4.3.2

--- toolchain-ios5-xcode.cmake
+++ toolchain-ios5-xcode.cmake
@@ -1,10 +1,12 @@
 #
 # iOS toolchain file for CMake
 #
-# (c) Copyrights 2010-2011 Hartmut Seichter
+# (c) Copyrights 2010-2012 Hartmut Seichter
 # 
-# Note: this version can only be used for Xcode 4.x (or Makefiles) and iOS 5 SDK 
-# 
+# Note: this version can only be used for Xcode 4.3 (or Makefiles) and iOS 5 SDK 
+#
+# March 24, 2012 - Updated for Xcode 4.3.2 and iOS SDK 5.1 
+#
 
 # force CMake to assume crosscompiling
 set(CMAKE_CROSSCOMPILING 1)
@@ -18,12 +20,17 @@ set(APPLE 1)
 set(IOS 1)
 
 # hard set values
-set(IOS_SDK_VERSION "5.0")
+set(IOS_SDK_VERSION "5.1")
 set(IOS_TARGET "iPhoneOS")
 set(IOS_ARCH "armv7")
 
+find_program(CMAKE_XCODE_SELECT xcode-select)
+if(CMAKE_XCODE_SELECT)
+	execute_process(COMMAND ${CMAKE_XCODE_SELECT} "-print-path" OUTPUT_VARIABLE OSX_DEVELOPER_ROOT OUTPUT_STRIP_TRAILING_WHITESPACE)
+endif()
+
 # some internal values
-set(IOS_DEVELOPER_ROOT "/Developer/Platforms/${IOS_TARGET}.platform/Developer")
+set(IOS_DEVELOPER_ROOT "${OSX_DEVELOPER_ROOT}/Platforms/${IOS_TARGET}.platform/Developer")
 set(IOS_SDK_ROOT "${IOS_DEVELOPER_ROOT}/SDKs/${IOS_TARGET}${IOS_SDK_VERSION}.sdk")
 
 # for Xcode 4.x we need to set the sysroot to the internal string
@@ -39,17 +46,26 @@ set(CMAKE_OSX_ARCHITECTURES "${IOS_ARCH}" CACHE STRING "SDK Architecture" FORCE)
 # default to searching for frameworks first
 set (CMAKE_FIND_FRAMEWORK FIRST)
 
-
 # set up the default search directories for frameworks
 set (CMAKE_SYSTEM_FRAMEWORK_PATH
 	${IOS_SDK_ROOT}/System/Library/Frameworks
-	${IOS_SDK_ROOT}/System/Library/PrivateFrameworks
 	${IOS_SDK_ROOT}/Developer/Library/Frameworks
+#	${IOS_SDK_ROOT}/System/Library/PrivateFrameworks
+	)
+
+# root path settings
+set(CMAKE_FIND_ROOT_PATH 
+	${IOS_DEVELOPER_ROOT}
+	${IOS_SDK_ROOT}
 	)
 
 # set appropriate flags
 set(CMAKE_C_FLAGS "--sysroot=${IOS_SDK_ROOT} -miphoneos-version-min=${IOS_SDK_VERSION} -arch ${IOS_ARCH} -v" CACHE STRING "C flags" FORCE)
 set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "C++ flags" FORCE)
+
+	
+
+#message(${CMAKE_FIND_ROOT_PATH})
 
 # specify compiler
 set(CMAKE_C_COMPILER   "${IOS_DEVELOPER_ROOT}/usr/bin/clang"    CACHE PATH "C compiler" FORCE)
@@ -61,14 +77,8 @@ if(XCODE)
 	set(CMAKE_C_COMPILER_WORKS TRUE)
 endif()
 
-# root path settings
-set(CMAKE_FIND_ROOT_PATH 
-	${IOS_DEVELOPER_ROOT} 
-	${IOS_SDK_ROOT}/usr
-	${IOS_SDK_ROOT}/System
-	)
 
 # search paths (for makefiles the first one might be switched to "NEVER")
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
